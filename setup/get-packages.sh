a3cc8fc50c8cc3ff17841008aecc8259f83348cf
neil stevenson <neil.git@secureness.co.uk>
2020-09-26 17:10:56 +0100

    Misc Raspberry OS setup scripts

---
+++ setup/get-packages.sh
@@ -0,0 +1,16 @@
+# Git
+#preinstalled: sudo apt-get install git
+
+#
+#preinstalled: sudo apt-get install pigpio python-pigpio python3-pigpio
+sudo apt-get install pigpiod
+sudo systemctl enable pigpiod
+
+# Prereqs for ApproxEng Redboard
+sudo apt-get install libtiff5 libopenjp2-7-dev fonts-dejavu libpython3-dev libjpeg-dev
+
+# Misc build
+sudo apt-get install gtkmm-3.0
+
+# for julius speech recognition
+sudo apt-get install build-essential zlib1g-dev libsdl2-dev libasound2-dev
---
+++ setup/get-python-libs.sh
@@ -0,0 +1,16 @@
+# Approx Eng libs
+pip3 install approxeng.input
+pip3 install redboard
+
+# Useful libs
+pip3 install simple_pid
+
+# Opencv add-ons
+pip3 install imutils
+pip3 install opencv-python
+pip3 install opencv-contrib-python
+pip3 install imageio
+pip3 install pytesseract
+pip3 install dlib
+pip3 install scipy
+
